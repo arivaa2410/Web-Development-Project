06b80bffdb4dc1aec9f6da464828067dd65cee65
LiyanaHut <154667007+LiyanaHut@users.noreply.github.com>
2023-12-31 15:35:00 +0800

    Update PageProfile.php

--- PageProfile.php
+++ PageProfile.php
@@ -249,7 +249,7 @@ body  {
         </div>
         <div class="text-right mt-3">
             <button type="button" class="btn btn-primary">Save changes</button>&nbsp;
-            <button type="button" class="btn btn-default">Cancel</button>
+            <button type="button" class="btn btn-default">Logout</button>
         </div>
     </div>
     <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
