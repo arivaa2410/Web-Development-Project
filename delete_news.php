3dde2c7825225cfce5368bcaa8e9b7712c1fb130
xiu0105 <152961710+xiu0105@users.noreply.github.com>
2023-12-31 21:54:44 +0800

    Update delete_news.php

--- delete_news.php
+++ delete_news.php
@@ -1,76 +1,35 @@
 <?php
+// delete_news.php
+
 // Assuming you have a database connection established
-// Include your database connection code here
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// Replace the following with your database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "webdev";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if the news ID is set and is a valid integer
-    if (isset($_POST['news_id']) && is_numeric($_POST['news_id'])) {
-        $newsId = $_POST['news_id'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Perform the deletion in the database
-        $sql = "DELETE FROM news WHERE news_id = ?";
-        $stmt = $conn->prepare($sql);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        // Bind the news ID parameter
-        $stmt->bind_param('i', $newsId);
+// Get the news_id from the AJAX request
+$newsId = isset($_POST['news_id']) ? $_POST['news_id'] : '';
 
-        // Execute the statement
-        if ($stmt->execute()) {
-            // Deletion successful
-            echo json_encode(['success' => true]);
-        } else {
-            // Deletion failed
-            echo json_encode(['success' => false, 'error' => 'Database error']);
-        }
+// Perform the deletion
+$sql = "DELETE FROM news WHERE news_id = '$newsId'";
 
-        // Close the statement
-        $stmt->close();
-    } else {
-        // Invalid news ID
-        echo json_encode(['success' => false, 'error' => 'Invalid news ID']);
-    }
+if ($conn->query($sql) === TRUE) {
+    // Deletion successful
+    echo "News deleted successfully";
 } else {
-    // Invalid request method
-    echo json_encode(['success' => false, 'error' => 'Invalid request method']);
+    // Error handling
+    echo "Error deleting news: " . $conn->error;
 }
 
 // Close the database connection
 $conn->close();
 ?>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var deleteButtons = document.getElementsByClassName('delete-btn');
-
-        Array.from(deleteButtons).forEach(function (button) {
-            button.addEventListener('click', function () {
-                var newsId = this.getAttribute('data-news-id');
-
-                if (confirm('Are you sure you want to delete this news?')) {
-                    // Send an AJAX request to delete_news.php
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('POST', 'delete_news.php', true);
-                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                    
-                    // Set up the callback function for when the request completes
-                    xhr.onload = function () {
-                        var response = JSON.parse(xhr.responseText);
-
-                        if (response.success) {
-                            // Deletion successful, you may want to update the table or reload the page
-                            window.location.reload();
-                        } else {
-                            // Deletion failed, handle the error
-                            alert('Error: ' + response.error);
-                        }
-                    };
-
-                    // Send the request with the news ID as POST data
-                    xhr.send('news_id=' + encodeURIComponent(newsId));
-                }
-            });
-        });
-    });
-</script>
